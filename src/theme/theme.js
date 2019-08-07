e5bb3f71274aaad8f4c65d30ad6fb22215614c61
vbojilova <viki.bojilova@gmail.com>
2019-08-07 14:53:30 -0400

    Upgraded material ui + created theme

---
+++ src/theme/theme.js
@@ -0,0 +1,15 @@
+import { createMuiTheme } from "@material-ui/core/styles";
+export const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#d44d2f"
+    },
+    secondary: {
+      main: "#1d83c6"
+    },
+    background: {
+      default: "#f7f7f5"
+    }
+  },
+  spacing: { unit: 4 }
+});
